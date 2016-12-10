4bf3a83146cd26d6a1055e5c81f13cf5e5cddb6c
Andros Fenollosa <andros@fenollosa.email>
2016-12-10 18:38:05 +0100

    Rename bills to bells

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local game = require 'assets/scripts/game'
 local stage1 = require 'assets/scripts/stage1'
 local controls = require 'assets/scripts/controls'
-local bills = require 'assets/scripts/bills'
+local bells = require 'assets/scripts/bells'
 local camera = require 'assets/scripts/camera'
 
 -- LOAD
@@ -9,7 +9,7 @@ function love.load()
 	game.load()
 	camera.load(game)
 	stage1.load(game, camera)
-	bills.load(game)
+	bells.load(game)
 	controls.load(game)
 end
 
@@ -18,7 +18,7 @@ function love.update(dt)
 	game.world:update(dt)
 	stage1.update(dt)
 	camera.update(game)
-	bills.update(dt)
+	bells.update(dt)
 	controls.update(dt, game)
 end
 
@@ -27,7 +27,7 @@ function love.draw()
 	camera.gcam:draw(
 		function(l,t,w,h)
 			stage1.draw()
-			bills.draw()
+			bells.draw()
 		end
 	)
 	controls.draw()
